e69d70834132c78826a4c9a7699f93047f6b635c
Sunny_Day <305964957@qq.com>
2017-11-19 21:08:47 +0800

    添加新内容

---
+++ gradleSpringBoot/src/main/java/com/mypro/beanDao/UserDao.java
@@ -0,0 +1,17 @@
+package com.mypro.beanDao;
+
+import com.mypro.bean.User;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface UserDao {
+    @Select("SELECT * FROM tms_user_info where user_id = #{id}")
+    @Results({
+            @Result(property = "user_id", column = "user_id"),
+            @Result(property = "user_name", column = "user_name"),
+            @Result(property = "password", column = "password")
+    })
+    User findById(@Param("user_id") int user_id);
+
+
+}
